391ec8699e256c1b5bf990e65c8e6838d88a10d2
Zixuan Li <armstrong0220@gmail.com>
2017-11-30 17:29:52 -0500

    update CU_Course_Directory.py

--- Web_Scraping/CU_Course_Directory.py
+++ Web_Scraping/CU_Course_Directory.py
@@ -10,10 +10,43 @@ Still in progress
 """
 
 import requests
+from bs4 import BeautifulSoup
 
-COMS = requests.get("http://www.columbia.edu/cu/bulletin/uwb/sel/COMS_Spring2018.html")
 
-COMS_text= requests.get("http://www.columbia.edu/cu/bulletin/uwb/subj/COMS/_Spring2018_text.html")
+def department_course_list(department):
+    """
+    Takes in a department four-letter abbreviation (e.g. COMS, STAT,IEOR)
+    
+    Return a list of strings of url for each course
+    """
+    
+    courseURL = []
+    site = requests.get("http://www.columbia.edu/cu/bulletin/uwb/subj/" + department + "/_Spring2018_text.html")
+    siteBS = BeautifulSoup(site.content, "html.parser")
+    
+    for link in siteBS.find_all("a"):
+        courseURL.append("http://www.columbia.edu" + link.get("href"))
+    
+    # The last one is for SIPA
+    if courseURL[-1] == "http://www.columbia.edu/cu/sipa/COURSES/":
+        return courseURL[:-1]
+    else:
+        return courseURL
+    
+    
+    
+    
+    
+    
+    
+    
+##############################################################################
+# Test Code Here
+# COMS = requests.get("http://www.columbia.edu/cu/bulletin/uwb/sel/COMS_Spring2018.html")
+# COMS_text= requests.get("http://www.columbia.edu/cu/bulletin/uwb/subj/COMS/_Spring2018_text.html")
 
 # Get course listing part
-COMS_list = COMS_text.text.split("<pre>")[1].split("</pre>")[0]
+# COMS_list = COMS_text.text.split("<pre>")[1].split("</pre>")[0]
+
+# COMS_bs = BeautifulSoup(COMS.content, "html.parser")    
+CS_course_URL = department_course_list("COMS")
