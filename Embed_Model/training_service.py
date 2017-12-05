8269895771b0fb32179b50bc0a6cca1579d02714
Achyles <cjq0715@gmail.com>
2017-12-05 14:23:29 -0500

    Update training_service.py
    Tested and commented the training service

--- Embed_Model/training_service.py
+++ Embed_Model/training_service.py
@@ -1,12 +1,21 @@
+'''
+This service takes in culpa and CU course directory data to train the word embedding model.
+The trained model is saved as a dictionary in json file.
+'''
+
 from embed_model import WordEmbed as WE
 import pandas as pd
 import re
+import json
 
+#define the department you working on here
+depart = 'COMS'
+
+#cleaning culpa data
 data =  pd.read_csv('../data/culpa.csv')
-
 cr = data[['Course','Rating']]
-
 course = []
+
 for i in range(cr.shape[0]):
     
     s = cr.at[i,'Course'].split()
@@ -21,22 +30,27 @@ for i in range(cr.shape[0]):
             s[1] = 'E'+s[1]
         else:
             s[1] = 'W'+s[1]
-            
+    
     course.append([s[0].upper(),s[1],cr.at[i,'Rating']])
 
+#store the rating data in a dataframe
 cscore = pd.DataFrame(course,columns=['department','course number','rating'])
 
-depart = 'COMS'
-courseData =  pd.read_csv('../data/'+depart+'_course_info.csv')
-
+#calculate the average rating of each course
 score = cscore.loc[cscore['department'].isin([depart])]
 score = score.groupby(['department','course number'])['rating'].mean().get(depart)
 keys = score.keys()
+
+#cleaning course directory data
+courseData =  pd.read_csv('../data/'+depart+'_course_info.csv')
 
 desc = courseData[['course number','description']]
 
 wordList = []
 scoreList = []
+
+#matching the description corpus with the rating score
+#stored in wordList and scoreList with indices matching
 for i in range(score.shape[0]):
     cnum = desc.at[i, 'course number']
     if cnum in keys:
@@ -44,6 +58,10 @@ for i in range(score.shape[0]):
         wordList.append(list(set(re.findall(r'\w+', desc.at[i, 'description'].lower()))))
         scoreList.append(score.get(cnum))
 
+#train the model
 weModel = WE()
 weModel.train(wordList, scoreList)
-print(weModel.predict('in'))
+
+#save the model as dictionary to json file
+with open('model.json','w') as fp:
+    json.dump(weModel.getModel(), fp)
