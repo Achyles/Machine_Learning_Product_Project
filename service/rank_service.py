0b7188ce6164a8bc765eefe5d3a8c4aa030821da
Achyles <cjq0715@gmail.com>
2017-12-07 15:03:35 -0500

    Add files via upload

--- service/rank_service.py
+++ service/rank_service.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 from evalue_service import *
 
-def rank_course(depart, cnums):
-	cnums = re.findall(r'\w+', cnums.upper())
-	
+def rank_course(inputstr):
+	inputstr = re.findall(r'\w+', inputstr.upper())
+	depart = inputstr[0]
+	cnums = inputstr[1:]
 	slist = []
 	for cnum in cnums:
 		slist.append([cnum, evaluate(depart, cnum)])
@@ -13,7 +14,7 @@ def rank_course(depart, cnums):
 	df = pd.DataFrame(slist, columns=['cnum','score'])
 	df = df.sort_values(by='score', ascending = False)
 	
-	return df['cnum'].tolist()
+	return df['cnum'].to_string()
 
 if __name__ == "__main__":
-	print(rank_course('COMS', 'w1004 w4995 w3136 '))
+	print(rank_course('COMS w1004 w4995 w3136 '))
