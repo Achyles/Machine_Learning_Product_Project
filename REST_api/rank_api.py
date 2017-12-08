50865c6231f7e84bddbae735b55a81e53fda4643
Achyles <cjq0715@gmail.com>
2017-12-07 20:58:18 -0500

    Add files via upload

--- REST_api/rank_api.py
+++ REST_api/rank_api.py
@@ -10,12 +10,13 @@ from rank_service import *
 """
 ranking service for input department and course numbers
 e.g.:
-localhost:7777/?rank=COMS+W4111+w1004+w4995
-
+localhost:7777/rank/?rank=COMS+W4111+w1004+w4995
 """
 
 class RankHandler(RequestHandler):
 	def get(self):
+		#take department + course_number input 
+		#e.g.: 'COMS w4111 w4995 w4170'
 		input = self.get_argument('rank','none')
 
 		self.write(rank_course(input))
@@ -26,8 +27,8 @@ class RankHandler(RequestHandler):
 
 if __name__ == "__main__":
 		handler_mapping = [
-							(r'/', RankHandler),
-						    ]
+							(r'/rank/', RankHandler),
+							]
 		application = Application(handler_mapping)
 		application.listen(7777)
 		IOLoop.current().start()
